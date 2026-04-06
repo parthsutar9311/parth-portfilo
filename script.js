b7b632cc22bcec542073721addc2a9241b470da4
parthsutar9311 <parthsutar5055@gmail.com>
2026-04-06 14:39:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+// Show welcome message
+window.onload = function() {
+    alert("Welcome to my portfolio website!");
+};
+
+// Button click message
+function showMessage() {
+    alert("Thanks for visiting my portfolio!");
+}
+
+// Dark mode toggle
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
